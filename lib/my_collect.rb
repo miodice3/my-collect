c75e08bd016e0bb496d441cc3911523839897025
Michael Iodice <miodice3@gmail.com>
2020-10-01 14:55:50 -0600

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+require 'pry'
 
+def my_collect(array)
+    i=0
+    modified = []
+    while i < array.length
+        var1 = yield array[i]
+        modified << var1
+        #binding.pry
+        i += 1
+    end
+    modified
+end
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
